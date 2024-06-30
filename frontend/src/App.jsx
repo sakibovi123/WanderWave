f36c97ea84a1b796945ad7058fe0957a5e7917bb
Sakib <sakibovi123@gmail.com>
2024-06-30 18:24:19 +0600

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+
+function App() {
+  
+  return (
+    <>
+    <Routes>
+      
+    </Routes>
+    </>
+  )
+}
+
+export default App
